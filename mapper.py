c9decf5c348b7e17218fd5ea9859808772725b88
LeMinaw <leminaw@gmail.com>
2017-02-25 17:20:05 +0100

    New mapping modes

--- mapper.py
+++ mapper.py
@@ -7,6 +7,28 @@ from bitstring import BitArray
 from math      import ceil, sqrt
 from os        import close
 
+
+def linear(x, xMin, xMax, yMin, yMax):
+    """Simple linear range conversion"""
+    return (x - xMin) * (yMax - yMin) / (xMax - xMin) + yMin
+
+
+def gcd(x, y):
+    """Simple greatest common divisor"""
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x%y)
+
+
+def lcm(x, y):
+    """Simple low common multiple"""
+    if x == 0 or y == 0:
+        return 0
+    else:
+        return (x * y) // gcd(x, y)
+
+
 def bitsFromFile(path):
     """Returns the binary data of a given file as a string of Os and 1s.\n
     path: String, the path to the file"""
@@ -17,7 +39,8 @@ def bitsFromFile(path):
 
     return BitArray(sourceData).bin
 
-def map(data, scale=1, ratio=1, multiple=1, name='generated'):
+
+def map(data, scale=1, ratio=1, multiple=1, cellX=1, cellY=1, mode='offset', name='generated'):
     """Returns a PNG image from a string of binay data.\n
     data: String, the input data (as 0s and 1s)
     scale: Int, the size of a data square (in px)
@@ -25,21 +48,67 @@ def map(data, scale=1, ratio=1, multiple=1, name='generated'):
     multiple: Int, the output image dimensions will be a multiple of it
     name: String, the output file  name (without extension)"""
 
-    sizeX = nceil(sqrt(len(data))*sqrt(ratio), multiple)
-    sizeY = nceil(sqrt(len(data))*(1/sqrt(ratio)), multiple)
+    cellRatio = cellX/cellY
+    if mode == 'offset':
+        sizeCorrectorX = cellX
+        sizeCorrectorY = cellY
+    else:
+        sizeCorrectorX = 1
+        sizeCorrectorY = 1
+
+    sizeX = nceil((sqrt(len(data))*   sqrt(ratio*cellRatio) )/sizeCorrectorX, lcm(multiple, cellX))
+    sizeY = nceil((sqrt(len(data))*(1/sqrt(ratio*cellRatio)))/sizeCorrectorY, 1)
+
+    # sizeX = nceil(sqrt(len(data))/sizeCorrectorX, multiple*cellX)
+    # sizeY = nceil(sqrt(len(data))/sizeCorrectorY, multiple*cellY)
 
     image = Image.new(mode='RGBA', size=(sizeX, sizeY), color='white')
 
     print("Processing...")
     oor = 0
-    for line in range(0, sizeY):
-        for pixel in range(0, sizeX):
-            try:
-                if data[line * sizeX + pixel] == '1':
-                    image.putpixel((pixel, line), (0, 0, 0))
-            except IndexError:
-                oor += 1
-        print("Processed line", line, "/", sizeY)
+
+    if mode == 'offset':
+        for line in range(0, sizeY):
+            for pixel in range(0, sizeX):
+                colorData = []
+                for y in range(0, cellY):
+                    chanData = 0
+                    for x in range(0, cellX):
+                        try:
+                            chanData += int(data[(line*cellY+y) * sizeX*cellX + (pixel*cellX+x)])
+                        except IndexError:
+                            oor += 1
+                            chanData = 0
+                    chanData = int(linear(chanData, 0, cellX, 0, 255))
+                    colorData.append(chanData)
+                image.putpixel((pixel, line), tuple(colorData))
+            print("Processed line", line, "/", sizeY)
+
+    elif mode == 'sliding':
+        for line in range(0, sizeY):
+            for pixel in range(0, sizeX):
+                try:
+                    colorData = []
+                    for y in range(int((1-cellY)/2), int((1+cellY)/2)):
+                        chanData = 0
+                        for x in range(int((1-cellX)/2), int((1+cellX)/2)):
+                            chanData += int(data[(line+y) * sizeX + pixel+x])
+                            chanData = int(linear(chanData, 0, cellX, 0, 255))
+                        colorData.append(chanData)
+                    image.putpixel((pixel, line), tuple(colorData))
+                except IndexError:
+                    oor += 1
+            print("Processed line", line, "/", sizeY)
+
+    else:
+        for line in range(0, sizeY):
+            for pixel in range(0, sizeX):
+                try:
+                    if data[line * sizeX + pixel] == '1':
+                        image.putpixel((pixel, line), (0, 0, 0))
+                except IndexError:
+                    oor += 1
+            print("Processed line", line, "/", sizeY)
 
     print("OOR:", oor)
     print("Saving...")
@@ -58,10 +127,11 @@ def nceil(x, n=1):
     """Returns the next n-multiple of a number.\n
     x: Float, the input number
     n: Float, will be the next multiple of this number"""
-
     y = ceil(x) - ceil(x) % n
     if y < x:
         y += n
+
+    print(str(x) + ":" + str(y))
     return y
 
 
@@ -71,4 +141,11 @@ if __name__=='__main__':
         data = bitsFromFile(f)
         #print(data)
         #print(percentOfChar(data, '1'))
-        map(data, scale=1, ratio=16/9, multiple=8, name=f)
+        #map(data, scale=4, ratio=16/9, multiple=1, cellX=64, cellY=3, mode='offset', name=f)
+
+        map(data, scale=4, ratio=16/9, multiple=16, cellX=1, cellY=1, mode='bin', name=f+'-bin')
+
+        map(data, scale=4, ratio=16/9, multiple=16, cellX=3, cellY=3, mode='sliding', name=f+'-sliding')
+
+        for integ in (64, 32, 24, 16, 12, 8, 6, 4, 2, 1):
+            map(data, scale=4, ratio=16/9, multiple=16, cellX=integ, cellY=3, mode='offset', name=f+'-'+str(integ))
